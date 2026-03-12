887aae60ac01b01c21bb82d2ac683697c8412bde
brunvermaa40-lang <brunvermaa40@gmail.com>
2026-03-12 11:46:46 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+// frontend/src/App.tsx
+import React from 'react';
+import { Dashboard } from './pages/Dashboard';
+import './App.css';
+
+function App() {
+  return <Dashboard />;
+}
+
+export default App;
